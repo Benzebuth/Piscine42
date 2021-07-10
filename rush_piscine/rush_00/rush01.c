f8a4f6df6e125265d5e0281abf8f5bbe42ca59e9
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-10 18:32:38 +0200

    rush 00 update

--- rush_piscine/rush_00/rush01.c
+++ rush_piscine/rush_00/rush01.c
@@ -6,35 +6,36 @@
 /*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 12:02:51 by bcolin            #+#    #+#             */
-/*   Updated: 2021/07/10 14:21:15 by bcolin           ###   ########.fr       */
+/*   Updated: 2021/07/10 17:54:38 by bcolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-#include "ft_putchar.c"
+void ft_putchar(char c);
 
 void	ft_print(int line, int column, int x_max, int y_max)
 {
-	if (line == 1)
+	if (line == 0)
 	{
-		if (column == 1)
+		if (column == 0)
 			ft_putchar('/');
-		else if (column == x_max)
+		else if (column == x_max-1)
 			ft_putchar('\\');
 		else
 			ft_putchar('*');
 	}
-	else if (line == y_max)
+	else if (line == y_max-1)
 	{
-		if (column == 1)
+		if (column == 0)
 			ft_putchar('\\');
-		else if (column == x_max)
+		else if (column == x_max-1)
 			ft_putchar('/');
 		else
 			ft_putchar('*');
 	}
 	else
 	{
-		if (column == 1 || column == x_max)
+		if (column == 0 || column == x_max-1)
 			ft_putchar('*');
 		else
 			ft_putchar(' ');
@@ -46,11 +47,11 @@ void	rush(int x, int y)
 	int	line;
 	int column;
 
-	line = 1;
-	while(line <= y)
+	line = 0;
+	while(line < y)
 	{
-		column = 1;
-		while(column <= x)
+		column = 0;
+		while(column < x)
 		{
 			ft_print(line, column, x, y);
 			column++;
