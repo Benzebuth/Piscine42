e9f0e51e29bed40d4229ed24934e6ca6dc8ebde4
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-10 22:22:59 +0200

    update cast unsigned ex07

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 16:51:23 by bcolin            #+#    #+#             */
-/*   Updated: 2021/07/09 00:39:23 by bcolin           ###   ########.fr       */
+/*   Updated: 2021/07/10 22:20:22 by bcolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,22 @@ void	ft_print(char c)
 
 void	ft_putnbr(int nb)
 {
-	if (nb >= 10)
+	unsigned int	u_nbr;
+
+	if (nb < 0)
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		u_nbr = (unsigned int)(nb * -1);
+		write(1, "-", 1);
+	}
+	else
+		u_nbr = (unsigned int)nb;
+	if (u_nbr >= 10)
+	{
+		ft_putnbr(u_nbr / 10);
+		ft_putnbr(u_nbr % 10);
 	}
 	else
 	{
-		ft_print(nb + '0');
+		ft_print(u_nbr + '0');
 	}
 }
