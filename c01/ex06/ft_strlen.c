42b317e3988c7c7b3b392e4a634cb101849ec244
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-11 18:51:27 +0200

    update ex06

--- c01/ex06/ft_strlen.c
+++ c01/ex06/ft_strlen.c
@@ -6,10 +6,9 @@
 /*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 16:07:36 by bcolin            #+#    #+#             */
-/*   Updated: 2021/07/11 18:49:18 by bcolin           ###   ########.fr       */
+/*   Updated: 2021/07/11 18:50:42 by bcolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
@@ -23,6 +22,5 @@ int	ft_strlen(char *str)
 		count++;
 		result++;
 	}
-	printf("%d\n", result);
 	return (result);
 }
