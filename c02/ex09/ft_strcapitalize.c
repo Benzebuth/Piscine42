d0b201848b95d3d9a01189852e0f1fb7979750b1
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-13 19:40:45 +0200

    add ex09

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/13 17:49:14 by bcolin            #+#    #+#             */
+/*   Updated: 2021/07/13 19:40:00 by bcolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_numeric(char *str, int c)
+{
+	if (str[c] >= '0' && str[c] <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_is_letter(char *str, int c)
+{
+	if (str[c] >= 'A' && str[c] <= 'Z')
+		return (1);
+	else if (str[c] >= 'a' && str[c] <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_is_lower(char *str, int c)
+{
+	if (str[c] >= 'a' && str[c] <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	c;
+	int	fl;
+
+	c = 0;
+	fl = 1;
+	while (str[c] != '\0')
+	{
+		if (ft_is_letter(str, c))
+		{
+			if (fl == 1)
+			{
+				if (ft_is_lower(str, c))
+				{
+					str[c] -= ' ';
+					fl = 0;
+				}
+			}
+		}
+		else if (ft_is_numeric(str, c))
+			fl = 0;
+		else
+			fl = 1;
+		c++;
+	}
+	return (str);
+}
