a74d91e3440d7097093004ae05cf628dd8425f54
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-15 23:56:12 +0200

    upgrade n in strncmp

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,32 +6,28 @@
 /*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 17:26:40 by bcolin            #+#    #+#             */
-/*   Updated: 2021/07/15 22:07:30 by bcolin           ###   ########.fr       */
+/*   Updated: 2021/07/15 23:55:35 by bcolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //#include <stdio.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int	c;
+	unsigned int	i;
 
-	c = 0;
-	while (c++ < n && *s1 && *s2)
+	i = 0;
+	while ((*(s1 + i) != '\0' || *(s2 + i) != '\0') && i < n)
 	{
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		else
-			return (*s1 - *s2);
+		if (! (*(s1 + i) == *(s2 + i)))
+			return (*(s1 + i) - *(s2 + i));
+		i++;
 	}
 	return (0);
 }
 /*
 int main()
 {
-	char s1[] = "tesa";
+	char s1[] = "aesa";
 	char s2[] = "tesb";
 	printf("%d", ft_strncmp(s1, s2, 4));
 }
