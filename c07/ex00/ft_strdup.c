d98d06103d89565916b1bd934a785f4706698704
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-19 23:51:10 +0200

    aller

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 22:18:35 by bcolin            #+#    #+#             */
+/*   Updated: 2021/07/19 23:17:57 by bcolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		len;
+	int		c;
+
+
+	len = ft_strlen(src);
+	dest = (char *)malloc(sizeof(char) * len);
+	if (dest == NULL)
+		return (NULL);
+	c = 0;
+	while (c < len)
+	{
+		dest[c] = src[c];
+		c++;
+	}
+	return (dest);
+}
+
+int	main()
+{
+	printf("%s", ft_strdup("coucoutest"));
+}
---
+++ c07/ex01/ft_range.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 23:25:19 by bcolin            #+#    #+#             */
+/*   Updated: 2021/07/19 23:50:11 by bcolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*tab;
+	int	i;
+
+	tab = (int *)malloc(sizeof(int) * (max - min));
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		min++;
+		i++;
+	}
+	return (tab);
+}
+/*
+int	main()
+{	
+	int i;
+	int *stock;
+
+	stock = ft_range(5, 15);
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d", stock[i++]);
+	}
+}
+*/
