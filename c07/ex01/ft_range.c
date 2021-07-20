657e9c7a828547a121892551c211907998b6c6c2
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-20 14:53:58 +0200

    cocou

--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 23:25:19 by bcolin            #+#    #+#             */
-/*   Updated: 2021/07/20 00:03:48 by bcolin           ###   ########.fr       */
+/*   Updated: 2021/07/20 14:53:46 by bcolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	*ft_range(int min, int max)
 	int	i;
 
 	tab = (int *)malloc(sizeof(int) * (max - min));
-	if (tab == NULL)
+	if (!*tab)
 		return (NULL);
 	i = 0;
 	while (min < max)
