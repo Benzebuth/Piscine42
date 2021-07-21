c6df0f539019e8dae95ad83040cdf284d02b0b18
Benjamin Colin <bcolin@student.42lausanne.ch>
2021-07-21 15:17:46 +0200

    add dir + ex00

---
+++ c08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcolin <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/21 15:04:46 by bcolin            #+#    #+#             */
+/*   Updated: 2021/07/21 15:17:03 by bcolin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcomp(char *s1, char *s2);
+
+#endif
